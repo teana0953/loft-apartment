9a00cf4a946827e4409a1c8fbb4d569b989d186c
Tina.Liang <teana0953@gmail.com>
2021-04-06 00:54:03 +0800

    Update server productionUrl

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 let developmentUrl = `http://localhost:50000/api/v1`;
 
-let productionUrl = 'https://pwa-demo0311.herokuapp.com';
+let productionUrl = 'https://loft-apartment.herokuapp.com/api/v1';
 
 export const baseUrl = process.env.NODE_ENV === 'production' ? productionUrl : developmentUrl;
 export const Server = Axios.create({
