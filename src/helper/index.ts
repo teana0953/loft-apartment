63d9d5dc5126acc4cf83149904c64997efe14b7e
Tina.Liang <teana0953@gmail.com>
2021-04-12 00:08:49 +0800

    Update helper, add t-axios-rxjs

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -6,3 +6,7 @@ export const ServiceWorkerService = new ServiceWorkerHelper();
 import { LocalStorageHelper } from './local-storage';
 export const LocalStorageService = new LocalStorageHelper();
 export * from './local-storage';
+
+import { Server } from '@/server';
+import { AxiosObservable } from 't-axios-rxjs';
+export const AxiosService = new AxiosObservable(Server);
